3338834f971f5e4a396ec64a5c6eee1cb1e8f7b1
Alwin Sunu <alwinsunu1974@gmail.com>
2026-03-27 11:17:36 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,47 @@
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Solution {
+
+    // Generic method to find the maximum in an array
+    // T extends Comparable<T> ensures the elements can be compared
+    public static <T extends Comparable<T>> T findMax(T[] array) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+
+        T max = array[0]; // Assume first element is max
+        for (int i = 1; i < array.length; i++) {
+            // compareTo returns > 0 if the current element is greater than max
+            if (array[i].compareTo(max) > 0) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // TODO: Read the integer N
-        
-        // TODO: Create a HashSet to store the integers
-        
-        // TODO: Loop N times to read the integers and add them to the HashSet
-        
-        // TODO: Iterate through the HashSet and print the unique numbers (space-separated)
-        
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+
+        // 1. Handle Integer Array
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            Integer[] intArray = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                intArray[i] = sc.nextInt();
+            }
+            System.out.println("Max Integer: " + findMax(intArray));
+        }
+
+        // 2. Handle String Array
+        if (sc.hasNextInt()) {
+            int m = sc.nextInt();
+            String[] strArray = new String[m];
+            for (int i = 0; i < m; i++) {
+                strArray[i] = sc.next();
+            }
+            System.out.println("Max String: " + findMax(strArray));
+        }
+
+        sc.close();
     }
 }
